81da8e1600fbe08e32d306b6c637a69f2565754b
Efrain Luciano Fernandez Valverde <efrain19091@gmail.com>
2023-01-25 06:06:53 +0000

    Add code to stop at sec1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
-
 let video = document.getElementById("cake-video"); 
-let button = document.getElementById("real");
+let button = document.getElementById("cakebtn");
 
-// Play / pause.
-button.addEventListener('click', function () {
+function playPause(){ 
     if (video.paused === false) {
         video.pause();
-        video.firstChild.nodeValue = 'Play';
     } else {
         video.play();
-        video.firstChild.nodeValue = 'Pause';
     }
-});
+}
+button.addEventListener('click', playPause)
+
+video.addEventListener('timeupdate', function() {
+    let videoCurrentTime = video.currentTime;
+        if (videoCurrentTime >= 1) {
+            video.pause();
+        } else {
+            video.play();
+        }
+    })
